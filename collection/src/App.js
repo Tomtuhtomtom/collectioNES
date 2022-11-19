1488d74045eb8c1fdf50f8216acb9b8bc9091a26
Thomas Morris <tomchrismorris@yahoo.com>
2022-11-19 12:52:12 -0500

    installs router axios local storage state, initial layout for login page

--- collection/src/App.js
+++ collection/src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>collectioNES</h1>
+      <div className="main-title">
+        <div className="row">
+          <div className="title-item"><h1>collectioNES</h1></div>
+          <div className="title-item"><button>Log In</button><button>Register</button></div>
+        </div>
+      </div>
     </div>
   );
 }
