cfdc2085c83b9219d5f23c144bccaf1b4186bbce
Thomas Morris <tomchrismorris@yahoo.com>
2022-11-26 19:19:21 -0500

    adds placeholder for username above title when logged in, additional styling

--- collection/src/App.js
+++ collection/src/App.js
@@ -9,8 +9,10 @@ const isLoggedIn = true
 function App() {
   return (
     <div className="App">
+      <div className="main-page-username-container">{isLoggedIn ? (
+          <h2 className="main-page-username">Username's</h2>) : (<h2 className="main-page-username"></h2>)}</div>
       <div className="main-page main-title">
-            <h1>collectioNES</h1>
+        <h1>collectioNES</h1>
       </div>
     <div className="main-page-menu">
       {!isLoggedIn ? (
--- collection/src/components/LoggedInMenu.js
+++ collection/src/components/LoggedInMenu.js
@@ -5,11 +5,11 @@ export class LoggedInMenu extends Component {
         return (
             <div className="main-menu">
                 <ul className="main-menu-list">
-                    <li className="main-menu-list-item"><a className="main-menu-link" href=''>Log Out</a></li>
                     <li className="main-menu-list-item"><a className="main-menu-link" href=''>New Collection</a></li>
                     <li className="main-menu-list-item"><a className="main-menu-link" href=''>Load Collection</a></li>
                     <li className="main-menu-list-item"><a className="main-menu-link" href=''>Official Collection</a></li>
                     <li className="main-menu-list-item"><a className="main-menu-link" href=''>Unlicensed Collection</a></li>
+                    <li className="main-menu-list-item"><a className="main-menu-link" href=''>Log Out</a></li>
                 </ul>
             </div>
         )
