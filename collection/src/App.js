7b94479f9a4861d92860bdde71bfd87a5e1f86c3
Thomas Morris <tomchrismorris@yahoo.com>
2022-12-03 19:12:33 -0500

    adds prep code for login

--- collection/src/App.js
+++ collection/src/App.js
@@ -1,12 +1,49 @@
-
 import './App.css';
-import {LoggedInMenu} from './components/LoggedInMenu';
-import {MainMenu} from './components/MainMenu'
 
-const isLoggedIn = true
+import {LoggedInMenu} from './components/LoggedInMenu'
+import {MainMenu} from './components/MainMenu'
+import {Login} from './components/Login'
+
+import { useState, useEffect } from 'react'
+import useLocalStorageState from 'use-local-storage-state'
+import { Routes, Route } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+// const isLoggedIn = true
 // const isLoggedIn = false
 
 function App() {
+  const [token, setToken] = useLocalStorageState('collectionToken', null)
+  const [username, setUsername] = useLocalStorageState('collectionUsername', '')
+  const navigate = useNavigate()
+
+  const setAuth = (username, token) => {
+    setToken(token)
+    setUsername(username)
+  }
+
+  const handleLogout = () => {
+    axios
+      .post(
+        'http://127.0.0.1:8000/auth/token/logout',
+        {},
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      )
+      .then(() => {
+        setAuth('', null)
+        navigate('/')
+      })
+  }
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const isLoggedIn = username && token
+
   return (
     <div className="App">
       <div className="main-page-username-container">{isLoggedIn ? (
--- collection/src/components/Login.js
+++ collection/src/components/Login.js
@@ -3,13 +3,69 @@ import { useState } from 'react';
 import axios from 'axios';
 import "../App.css";
 // import { Register } from "./register";
-import { useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 
 
-export class Login extends Component {
-    render() {
-        return (
-            <div>Login</div>
-        )
+export const Login = ({ setAuth, isLoggedIn }) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setError(null)
+
+        axios
+            .post('http://127.0.0.1:8000/auth/token/login/', {
+                username: username,
+                password: password,
+            })
+            .then((res) => {
+                const token = res.data.auth_token
+                setAuth(username, token)
+                navigate('/')
+            })
+            .catch((error) => {
+                setError(error.message)
+            })
     }
+    if (!isLoggedIn){
+    return (
+        <div>
+        <h1>test</h1>
+        { error && <p className='error'>{error} </p> }
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='username-input'>Username: </label>
+                <input
+                    type='text'
+                    id='username-input'
+                    value={username}
+                    required
+                    onChange = {(e) => setUsername(e.target.value)}
+                ></input>
+            </div>
+            <div>
+                <label htmlFor='password-input'>Password: </label>
+                <input
+                    type='password'
+                    id='password-input'
+                    value={password}
+                    required
+                    onChange = {(e) => setPassword(e.target.value)}
+                ></input>
+            </div>
+            <div>
+                <input type='submit' className='btn' value='Log In' />
+            </div>
+        </form>
+        <div>
+            <Link to="change to '/register' when the component is made"> 
+                Register
+            </Link>
+        </div>
+        </div>
+    )
+}
 }
